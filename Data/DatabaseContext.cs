40eeff5fd0a7261ab018dfc4a06d669ae6c5bcb0
Szabó Kristóf <szabokristof997@gmail.com>
2021-07-12 17:48:14 +0200

    added Models and DbCcontext

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using HotelListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,10 @@ namespace HotelListing
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<DatabaseContext>(options =>
+
+                options.UseSqlServer(Configuration.GetConnectionString("sqlConnection")));
 
             services.AddControllers();
 
